d9ee722df3ec5a4e50e8cc1a2a5cfe5f0f2205b2
jollySleeper <git@pm21.anonaddy.com>
2024-04-24 08:03:56 +0530

    Updated: AdGuardHome: Script

--- apps/adguardhome/podman-setup.sh
+++ apps/adguardhome/podman-setup.sh
@@ -4,10 +4,17 @@ source ../../scripts/common.sh
 
 NAME="adguardhome"
 IMAGE_SOURCE="docker.io/adguard/$NAME:latest"
+action_based_on_query "$1-con" "$NAME" "$IMAGE_SOURCE"
 
 echo "-> Making Required Directories"
 mkdir -pv "$(get_vol_dir ${NAME})/work"
 
+# Making Files Data for Rootless Container; Reqd by Both Folders
+files=$(ls -A $(get_vol_dir ${NAME})/work)
+if [[ $? == 0 ]] && [[ -z "$files" ]]; then
+    source ./scripts/rootless-data.sh
+fi
+    
 echo "-> Running '$NAME' Container"
 # Experimenting with DHCP
     #--network host \ Network = HOST Not reqd for preserving IPs
