96d5cb877e55f29e47cf11c195b252782806bccf
jollySleeper <git@pm21.anonaddy.com>
2024-04-28 12:46:10 +0530

    Updated: Scripts: Doc for SystemD Service

--- scripts/systemd/service.sh
+++ scripts/systemd/service.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Usage
+#   `./podman-setup.sh`
+#       Will Try to Install and Start All the Containers in App Folder 
+#   `./podman-setup.sh 'start'`
+#       Will Start All the Containers in App Folder 
+#   `./podman-setup.sh 'stop'`
+#       Will Stop All the Containers in App Folder 
+
 for folder in ~/selfhost/apps/*; do
     if [[ "$folder" != *".bak" ]] && [[ -f "$folder/podman-setup.sh" ]]; then
         cd "$folder"
