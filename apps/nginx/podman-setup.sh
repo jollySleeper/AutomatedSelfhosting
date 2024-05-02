c6a6bd3cb067a016767e89069b846334fc620daf
jollySleeper <git@pm21.anonaddy.com>
2024-05-02 10:45:33 +0530

    Updated: Nginx: Testing Container

--- apps/nginx/podman-setup.sh
+++ apps/nginx/podman-setup.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
+source ../../scripts/common.sh
+
+NAME="nginx"
+IMAGE_SOURCE="docker.io/library/$NAME:stable-alpine"
+
+action_based_on_query "$1-con" "$NAME" "$IMAGE_SOURCE"
+
+echo "-> Making Required Directories"
+#mkdir -pv "$(get_vol_dir ${NAME})/lib/$NAME"
+
+echo "-> Running '$NAME' Container"
+podman run \
+ --detach \
+ --restart unless-stopped \
+ --label io.containers.autoupdate=registry \
+ --network slirp4netns:port_handler=slirp4netns,allow_host_loopback=true `#Allows 127.0.0.1 of Host in Container` \
+ -p 80:1080 \
+ -p 443:1433 \
+ --name "$NAME" \
+ "$IMAGE_SOURCE"
+
 echo "Done :)"
