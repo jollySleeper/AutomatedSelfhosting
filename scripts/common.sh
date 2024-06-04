1260ce3ea67ecbde2a1f2861d71351e8565195fd
jollySleeper <git@pm21.anonaddy.com>
2024-06-04 09:25:58 +0530

    Updated: Script: Common: Trying to Automate Building Nginx Server Conf

--- scripts/common.sh
+++ scripts/common.sh
@@ -28,6 +28,22 @@ function source_container() {
     source ${REL_DIR}/podman/container.sh
 }
 
+function generate_nginx_conf_file() {
+    subdomain=${1}
+    domain=${2}
+    tld=${3}
+    port=${4}
+    protocol=${5}
+    template="$SELFHOSTED_APPS_PATH/nginx/configs/site-templates/$protocol-template.conf"
+    conf_file="$SELFHOSTED_APPS_PATH/nginx/configs/sites/$subdomain.conf"
+
+    echo "-> Making Nginx Confg File for '$subdomain'"
+    cp "$template" "$conf_file"
+    sed -i "s|{{subdomain}}|$subdomain|g" "$conf_file"
+    sed -i "s|{{domain}}|$domain|g" "$conf_file"
+    sed -i "s|{{tld}}|$tld|g" "$conf_file"
+    sed -i "s|{{port}}|$port|g" "$conf_file"
+}
 function action_based_on_query() {
     appName=${2}
     imageSource=${3}
@@ -40,6 +56,9 @@ function action_based_on_query() {
         "stop-con")
             source_container
             stop_container_and_exit "$appName"
+            ;;
+        "generate-nginx-conf-file")
+            generate_nginx_conf_file "$appName" ${3} ${4} ${5} ${6}
             ;;
         "-con")
             # Install
