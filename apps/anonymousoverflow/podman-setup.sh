eb036e67651ff4ca506f8d098c1a6193e3c0ca0d
jollySleeper <git@pm21.anonaddy.com>
2024-07-02 08:33:03 +0530

    Added: AnonymousOverflow: Build Image Manually

---
+++ apps/anonymousoverflow/podman-setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source ../../scripts/common.sh
+
+NAME="anonymousoverflow"
+IMAGE_SOURCE="localhost/httpjamesm/$NAME:latest"
+
+action_based_on_query "$1-con" "$NAME" "$IMAGE_SOURCE"
+
+echo "-> No Required Directories"
+
+echo "-> Running '$NAME' Container"
+podman run \
+ --detach \
+ --restart unless-stopped \
+ --label io.containers.autoupdate=local \
+ --user $(id -u):$(id -g) \
+ --env-file "$(get_env_dir ${NAME})/local.env" \
+ -p ${LOCALHOST_IP}:8011:8080 \
+ --name "$NAME" \
+ "$IMAGE_SOURCE"
+
+action_based_on_query "generate-nginx-conf-file" "$NAME" "aevion" "lan" "8011" "http"
+
+echo "Done :)"
