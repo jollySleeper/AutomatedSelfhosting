765fbbebfeb1e95a1fc9165acb2a455ce718bc27
jollySleeper <git@pm21.anonaddy.com>
2024-07-15 10:05:12 +0530

    Added: Script: Image: For Image Operations Like Building

---
+++ scripts/podman/image.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function check_image () {
+    echo "-> Checking for Existing '$1' Image"
+    if [[ -z $(podman images -q "$1") ]]; then
+        echo "-> Image Doesnot Exsits"
+        export status=1
+    else
+        echo "-> Image Already Exsits"
+        export status=0
+    fi 
+}
+
+function build_image() {
+    check_image "$2"
+    if [[ ${status} != 0 ]]; then
+        echo "-> Building '$1' Image"
+        cp docker/Dockerfile .
+        podman build -f Dockerfile
+        if [[ $? == 0 ]]; then
+            podman tag $(podman images | awk '{print $3}' | awk 'NR==2') "$2"
+        fi
+        rm Dockerfile
+    fi
+}
