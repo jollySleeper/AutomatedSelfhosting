1a025d6c6c17ee2d9947bf1f67feae978f9d89b1
jollySleeper <git@pm21.anonaddy.com>
2024-07-25 08:49:15 +0530

    Added: App: Redlib

---
+++ apps/redlib/podman-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source ../../scripts/common.sh
+
+NAME="redlib"
+IMAGE_SOURCE="quay.io/$NAME/$NAME:latest"
+
+action_based_on_query "$1-con" "$NAME" "$IMAGE_SOURCE"
+
+echo "-> No Required Directories"
+
+echo "-> Running '$NAME' Container"
+podman run \
+ --detach \
+ --restart unless-stopped \
+ --label io.containers.autoupdate=registry \
+ --user $(id -u):$(id -g) \
+ -p ${LOCALHOST_IP}:8013:8080 \
+ --name "$NAME" \
+ "$IMAGE_SOURCE"
+
+# action_based_on_query "generate-nginx-conf-file" "$NAME" "aevion" "lan" "8013" "http"
+
+echo "Done :)"
